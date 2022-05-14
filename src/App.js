2cdba971ca1ec54379a7f2bfdf26253d3d7e20c4
robonexx <robertwagar@gmail.com>
2022-05-14 08:31:11 +0200

    Trying some black, darkish, red styling... fear of the darkside?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-
 import './App.css';
+import Rob1 from './Rob1';
+import RobBg from './RobBg';
+import Rob12 from './Rob1';
+import Rob2Bg from './RobBg';
+
+import { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div className="App">
-      <h1> New portfolio</h1>
-    </div>
+    <motion.div className='App'>
+      <motion.h1>FEAR OF THE DARKSIDE?</motion.h1>
+
+      <div className='bg'>
+        <div className='image_bg'>
+          <RobBg />
+        </div>
+        <div className='image'>
+          <Rob1 />
+        </div>
+      </div>
+
+    
+        <div className='bg2'>
+          <div className='image_bg2'>
+            <Rob2Bg />
+          </div>
+          <div className='image2'>
+            <Rob12 />
+          </div>
+        </div>
+     
+    </motion.div>
   );
 }
 
