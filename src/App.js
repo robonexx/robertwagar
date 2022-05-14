56240587d64ec514f407ba0d9479031d4bdd2efb
robonexx <robertwagar@gmail.com>
2022-05-14 10:02:52 +0200

    Love this framer motion, could keep on forever, crazy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,93 @@
-import './App.css';
+import './App.scss';
 import Rob1 from './Rob1';
 import RobBg from './RobBg';
-import Rob12 from './Rob1';
-import Rob2Bg from './RobBg';
 
 import { motion } from 'framer-motion';
+const transition = { duration: 1, ease: [0.6, 0.01, -0.05, 0.9] };
 
+const firstWord = {
+  initial: {
+    y: 0,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      delayChildren: 0.6,
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const lastWord = {
+  initial: {
+    y: 0,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      delayChildren: 0.8,
+      staggerChildren: 0.1,
+      staggerDirection: -1,
+    },
+  },
+};
+const letter = {
+  initial: {
+    y: -400,
+  },
+  animate: {
+    y: 0,
+    transition: { duration: 1.2, ...transition },
+  },
+};
+const letter2 = {
+  initial: {
+    y: 400,
+  },
+  animate: {
+    y: 0,
+    transition: { duration: 1.2, ...transition },
+  },
+};
 function App() {
   return (
     <motion.div className='App'>
-      <motion.h1>FEAR OF THE DARKSIDE?</motion.h1>
+      <div className='wrapper'>
+        <motion.h1
+          initial={{ x: 0, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.4, duration: 2 }}
+        >
+          <motion.span>FEAR OF THE</motion.span>
+          <motion.span> DARKSIDE ?</motion.span>
+        </motion.h1>
+        <motion.div initial='initial' animate='animate' exit='exit'>
+          <motion.span className='text-animation first' variants={firstWord}>
+            <motion.span variants={letter}>F</motion.span>
+            <motion.span variants={letter}>E</motion.span>
+            <motion.span variants={letter}>A</motion.span>
+            <motion.span variants={letter}>R</motion.span>{' '}
+            <motion.span variants={letter}>O</motion.span>
+            <motion.span variants={letter}>F</motion.span>{' '}
+            <motion.span variants={letter}>T</motion.span>
+            <motion.span variants={letter}>H</motion.span>
+            <motion.span variants={letter}>E</motion.span>
+          </motion.span>
+          <br /> <br />
+          <motion.span className='text-animation last' variants={lastWord}>
+            <motion.span variants={letter2}>D</motion.span>
+            <motion.span variants={letter2}>A</motion.span>
+            <motion.span variants={letter2}>R</motion.span>
+            <motion.span variants={letter2}>K</motion.span>
+            <motion.span variants={letter2}>S</motion.span>
+            <motion.span variants={letter2}>I</motion.span>
+            <motion.span variants={letter2}>D</motion.span>
+            <motion.span variants={letter2}>E</motion.span>
+            <motion.span variants={letter2}>?</motion.span>
+          </motion.span>
+        </motion.div>
+      </div>
 
       <div className='bg'>
         <div className='image_bg'>
@@ -19,17 +97,6 @@ function App() {
           <Rob1 />
         </div>
       </div>
-
-    
-        <div className='bg2'>
-          <div className='image_bg2'>
-            <Rob2Bg />
-          </div>
-          <div className='image2'>
-            <Rob12 />
-          </div>
-        </div>
-     
     </motion.div>
   );
 }
