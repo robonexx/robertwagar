ac4244b01862bfd6ccdad177ec0ec2275db88cae
robonexx <robertwagar@gmail.com>
2022-05-16 16:06:43 +0200

    nav fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,8 +6,11 @@ import NavItem from '..//nav/NavItem';
 import styles from '../../styles/layout/Header.module.scss';
 import { AiOutlineHome } from 'react-icons/ai';
 import { SiMusicbrainz } from 'react-icons/si';
-import { MdOutlineHistoryEdu, MdOutlineAnnouncement } from 'react-icons/md';
-import { GiPlantRoots } from 'react-icons/gi';
+
+import { GiDodging } from 'react-icons/gi';
+import { GiTechnoHeart } from 'react-icons/gi';
+import { GiTargeted } from 'react-icons/gi';
+import { GiSkills } from 'react-icons/gi';
 
 export default function Header() {
   return (
@@ -15,14 +18,10 @@ export default function Header() {
       <TopMenu />
       <Nav>
         <NavItem title='home' icon={<AiOutlineHome />} url='/' />
-        <NavItem title='about' icon={<MdOutlineAnnouncement />} url='/about' />
-
-        <NavItem
-          title='work'
-          icon={<MdOutlineHistoryEdu />}
-          url='/work'
-        ></NavItem>
-        <NavItem title='Connect' icon={<SiMusicbrainz />} url='/connect' />
+        <NavItem title='about' icon={<GiDodging />} url='/about' />
+        <NavItem title='work' icon={<GiTargeted />} url='/work'></NavItem>
+        <NavItem title='dev' icon={<GiSkills />} url='/dev' />
+        <NavItem title='dance' icon={<GiTechnoHeart />} url='/dance' />
       </Nav>
     </div>
   );
--- src/components/topmenu/TopMenu.js
+++ src/components/topmenu/TopMenu.js
@@ -1,6 +1,6 @@
 import Logo from '../logo/Logo';
 import ContactBtn from '../contactBtn/ContactBtn';
-import { AiFillMail } from 'react-icons/ai';
+import { GiVibratingSmartphone } from 'react-icons/gi';
 
 // styles
 import styles from '../../styles/components/TopMenu.module.scss';
@@ -10,7 +10,7 @@ export default function TopMenu() {
     <div className={styles.top_menu}>
       <Logo />
       <h3 className={styles.subtitle}>Cr3aTiv3 M1nDz</h3>
-      <ContactBtn title='Contact' icon={<AiFillMail />} url='/contact' />
+      <ContactBtn title='Contact' icon={<GiVibratingSmartphone />} url='/contact' />
     </div>
   );
 }
